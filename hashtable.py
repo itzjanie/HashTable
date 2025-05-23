1a38c15fbefaac65ddc5e3cd0155bee3a2e34f01
itzjanie <fu.janie.huijia_2025@nyjc.edu.sg>
2025-05-23 03:13:51 +0000

    test

--- hashtable.py
+++ hashtable.py
@@ -1,6 +1,7 @@
 class HashTable:
     def __init__(self, size):
-        pass
+        self.size = size
+        self.values = [None] * self.size
 
     def __repr__(self):
         """returns a formatted string containing the values in the hash table"""
@@ -20,19 +21,48 @@ class HashTable:
         - key: str
           The key to be hashed
         """
-        pass
+        p = 31
+        m = 10^9 + 9
+        hashvalue = 0
+        i = 1
+        while i < len(key):
+            charvalue = ord(key[i]) - ord('a') + 1
+            hashvalue = hashvalue + (charvalue * (p^i))
+            i += 1
+        hashvalue = hashvalue % m
+        return hashvalue % self.size
+
 
     def setitem(self, key: str, value: dict) -> None:
         """
+        adds or updates an item and displays an appropriate message
         """
-        pass
+        hashvalue = self._hash(key)
+        if self.values[hashvalue] == None:
+            self.values[hashvalue] = value
+            print('Data successfully added.')
+        else:
+            self.values[hashvalue] = value
+            print('Data successfully updated.')
 
     def getitem(self, key: str) -> 'dict | None':
         """
+        returns an item in the hashtable, prints an error message if destination is empty and return None
         """
-        pass
+        hashvalue = self._hash(key)
+        if self.values[hashvalue] == None:
+            print('Destionation is empty')
+            return None
+        else:
+            return self.values[hashvalue]
         
     def delitem(self, key: str) -> None:
         """
+        removes the item from the destination, prints error message if destination is empty or otherwise
         """
-        pass
+        hashvalue = self._hash(key)
+        if self.values[hashvalue] == None:
+            print('Unable to remove. Destination is empty.')
+        else:
+            self.values[hashvalue] = None
+            print('Data successfully removed.')
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from hashtable import HashTable
 
 if __name__ == "__main__":
     ht = HashTable(30)
+    with open('student_data.csv', 'r') as fhand:
+        fhand.readline()
+        for line in fhand:
+            line = line.strip().split(',')
+            ht.setitem(line[1], line)
+    print(ht)
+    print(ht.getitem('s0039e'))
+    ht.delitem('s0040f')
+    print(ht)
     """
     1. Extract the records from the student_data file
     and add them one at a time, as a Python dict, 
